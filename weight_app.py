e42995aa9ddc0f825e1f50afe81df7a952173893
Daniel Ferreira <danielblindta@yahoo.com.br>
2025-07-03 23:58:31 -0400

    cosmetics

--- weight_app.py
+++ weight_app.py
@@ -20,7 +20,7 @@ with tab1:
         for m in missing:
             st.markdown(m.date())
 
-    col1, col2, col3 = st.columns([0.1, 0.1, 1])
+    col1, col2, col3 = st.columns([1, 1, 17])
     with col1:
         if st.button('Kgs', key='tab1_kgs_button'):
             analysis.change_measurement('kgs')
@@ -39,33 +39,32 @@ with tab1:
     
 with tab2:
     st.markdown('## Input Data')
-    col1, col2 = st.columns([1, 4])
+    col1, col2, col3, col4 = st.columns([1, 1, 1, 5])
     with col1:
         date = st.date_input("Select Date", value=None)
-        weight = st.number_input("Enter your weight (lbs)", min_value=0.0, step=0.1)
+    with col2:
+        weight = st.number_input("Enter your weight (lbs)", min_value=0.0, step=0.2)
+    with col3:
         food_score = st.number_input("Enter your food score", min_value=0, step=1)
-        exercise = st.checkbox("Did you exercise yesterday?")
-        if st.button("Update table"):
-            st.markdown(analysis.update_data(date, weight, food_score, exercise))
+    exercise = st.checkbox("Did you exercise yesterday?")
+    if st.button("Update table"):
+        st.markdown(analysis.update_data(date, weight, food_score, exercise))
 
 with tab3:
-    col1, col2, col3 = st.columns([0.09, 0.09, 1])
-    with col1:
+    col1, col2, col3 = st.columns([1, 1, 15])
+    with col1:   
         if st.button('Kgs', key='tab3_kgs_button'):
             analysis.change_measurement('kgs')
-            st.session_state.clear()
-
+            st.session_state.clear()                              
     with col2:
         if st.button('Lbs', key='tab3_lbs_button'):
             analysis.change_measurement('lbs')
-            st.session_state.clear()
-    with col3:
-        if st.button('Refresh', key='tab3_refresh_button'):
-            st.session_state.clear()
-    
-    col1, col2 = st.columns([1.25, 4])
+            st.session_state.clear()                                  
+    col1, col2 = st.columns([1, 8])
     with col1:
-        weeks = st.number_input("Number of weeks to forecast", min_value=1, max_value=10, value=2, step=1, key="week_input")
+        weeks = st.number_input("Weeks?", min_value=1, max_value=10, value=2, step=1, key="week_input")
+    if st.button('Refresh', key='tab3_refresh_button'):
+            st.session_state.clear()
     plot = analysis.forecast_graph(weeks)
     st.pyplot(plot)
 
@@ -74,7 +73,7 @@ with tab4:
     col1, col2 = st.columns([1, 4])
     with col1:
         n = st.slider("How many days?", min_value=5, max_value=100, value=20, step=5, key="last_n_slider")
-    col1, col2 = st.columns([0.05, 0.5])
+    col1, col2 = st.columns([1, 17])
     last = None
     with col1:
         if st.button('Kgs', key='tab4_kgs_button'):
